5ede5a3d8e121eabfd46592a8b977f9a0bc32e37
qbic22562 <mateusz.kubica99@gmail.com>
2020-09-21 19:21:28 +0200

    Fixed issue with unhandled exception raised by inverter.py functions

--- virtual_tlc10000_server/inverter.py
+++ virtual_tlc10000_server/inverter.py
@@ -27,7 +27,7 @@ def recalculate_values(value, unit):
         value = float(value) * 1000
     elif(unit == 'GWh'):
         value = float(value) * 1000000
-    return value
+    return float(value)
 
 
 def request_inverter():
@@ -37,6 +37,7 @@ def request_inverter():
         request_body = requests.get(INVERTER_URL, timeout=5)
     except requests.exceptions.ConnectionError:
         logger.warning('Cannot connect to inverter')
+        raise requests.exceptions.ConnectionError
     else:
         return request_body
 
@@ -46,25 +47,37 @@ def request_api():
 
     try:
         request_body = requests.get(API_URL, params = { 'key': API_KEY })
-    except requests.exceptions.ConnectionError as err:
+    except requests.exceptions.ConnectionError:
         logger.warning('Cannot connect to API')
+        raise requests.exceptions.ConnectionError
     else:
         return request_body
 
 
 def get_power():
-    request_body = request_inverter()
-    parameter_list = list(request_body.text.split('\n'))
-    return float(int(parameter_list[10]) / 1000)
+    try:
+        request_body = request_inverter()
+    except requests.exceptions.ConnectionError:
+        return 0.0
+    else:
+        parameter_list = list(request_body.text.split('\n'))
+        return float(int(parameter_list[10]) / 1000)
 
 
 def get_energy_today():
-    request_body = request_inverter()
-    parameter_list = list(request_body.text.split('\n'))
-    return float(parameter_list[11])
+    try:
+        request_body = request_inverter()
+    except requests.exceptions.ConnectionError:
+        return 0.0
+    else:
+        parameter_list = list(request_body.text.split('\n'))
+        return float(parameter_list[11])
 
 
 def get_energy_total():
-    request_body = request_api()
-    return recalculate_values(json.loads(request_body.text)['E-Total']['value'], json.loads(request_body.text)['E-Total']['unit'])
-
+    try:
+        request_body = request_api()
+    except requests.exceptions.ConnectionError:
+        return 0.0
+    else:
+        return recalculate_values(json.loads(request_body.text)['E-Total']['value'], json.loads(request_body.text)['E-Total']['unit'])
