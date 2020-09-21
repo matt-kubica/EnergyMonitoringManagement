3ac42945aa3d89f54134c0789349c6b2fd3b387c
qbic22562 <mateusz.kubica99@gmail.com>
2020-09-21 19:33:27 +0200

    Got rid of unnecesary script

--- docker-clear-all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker kill $(docker ps -q) # stop all containers
-docker rm $(docker ps -a -q) # remove all containers 
-docker rmi $(docker images -q) # remove all images
-docker network prune # remove all networks
-docker volume prune # remove all volumes
