e2e616a6b5422069c95a3fc1faca94d678fb0a2c
qbic22562 <mateusz.kubica99@gmail.com>
2020-09-25 10:32:24 +0200

    Modified virtual_tlc10000/inverter.py so theres no unhandled exceptions anymore

--- virtual_tlc10000_server/inverter.py
+++ virtual_tlc10000_server/inverter.py
@@ -37,7 +37,13 @@ def request_inverter():
         request_body = requests.get(INVERTER_URL, timeout=5)
     except requests.exceptions.ConnectionError:
         logger.warning('Cannot connect to inverter')
-        raise requests.exceptions.ConnectionError
+        return None
+    except requests.exceptions.ReadTimeout:
+        logger.warning('Reading from inverter timed out')
+        return None
+    except Exception as exc:
+        logger.error('Unusual exception:\n{0}'.format(exc))
+        return None
     else:
         return request_body
 
@@ -49,25 +55,29 @@ def request_api():
         request_body = requests.get(API_URL, params = { 'key': API_KEY })
     except requests.exceptions.ConnectionError:
         logger.warning('Cannot connect to API')
-        raise requests.exceptions.ConnectionError
+        return None
+    except requests.exceptions.ReadTimeout:
+        logger.warning('Reading from API timed out')
+        return None
+    except Exception as exc:
+        logger.error('Unusual exception:\n{0}'.format(exc))
+        return None
     else:
         return request_body
 
 
 def get_power():
-    try:
-        request_body = request_inverter()
-    except requests.exceptions.ConnectionError:
+    request_body = request_inverter()
+    if not requests_body:
         return 0.0
     else:
         parameter_list = list(request_body.text.split('\n'))
         return float(int(parameter_list[10]) / 1000)
-
+    
 
 def get_energy_today():
-    try:
-        request_body = request_inverter()
-    except requests.exceptions.ConnectionError:
+    request_body = request_inverter()
+    if not requests_body:
         return 0.0
     else:
         parameter_list = list(request_body.text.split('\n'))
@@ -75,9 +85,8 @@ def get_energy_today():
 
 
 def get_energy_total():
-    try:
-        request_body = request_api()
-    except requests.exceptions.ConnectionError:
+    request_body = request_api()
+    if not requests_body:
         return 0.0
     else:
         return recalculate_values(json.loads(request_body.text)['E-Total']['value'], json.loads(request_body.text)['E-Total']['unit'])
